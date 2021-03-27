5c44c67aa24ac30842ee7bcae187cb64da0f096b
Arek Chmenia <arkadiusz.chmenia@gmail.com>
2021-03-27 21:35:23 +0100

    Record/ stop/ play by one channel

--- project_2/src/index.ts
+++ project_2/src/index.ts
@@ -1,76 +1,26 @@
-interface IAudios {
-    [key: string]: HTMLAudioElement;
-} 
+import Keyboard from "./Keyboard.js";
+import Recorder, { RecorderState } from "./Recorder.js";
+import DefaultKeyboardMapper from "./mappers/DefaultKeyboardMapper.js";
+import Player from "./Player.js";
 
-interface IMapper {
-    [key: string]: string;
-}
-
-class Keyboard
-{
-    audios: IAudios = {};
-    key: (string | null) = null;
-
-    load(): void
-    {
-        let keyTypes = ["boom", "clap", "hihat", "kick", "openhat", "ride", "snare", "tink", "tom"];
-        let audios: IAudios  = {};
-
-        keyTypes.forEach(keyType => {
-            let sound: (HTMLAudioElement | null) = document.querySelector(`[data-sound="${keyType}"]`);
-
-            if(sound != null) {
-                audios[keyType] = sound;
-            }
-        });
-
-        this.audios = audios;
-    }
-
-    setKey(key: string) {
-        this.key = key;
-    }
-
-    play()
-    {
-        if(this.key != null && (this.key in this.audios)) {
-            let a = this.audios[this.key];
-            a.currentTime = 0;
-            a.play();
-        }
-    }
-}
-
-class DefaultKeyboardMapper
-{
-    static get mapper(): IMapper
-    {
-        return {
-            '1': "boom",
-            '2': "clap",
-            '3': "hihat",
-            '4': "kick",
-            '5': "openhat",
-            '6': "ride",
-            '7': "snare",
-            '8': "tink",
-            '9': "tom",
-        };
-    }
-}
-
+const recorder = new Recorder();
 const keyboard = new Keyboard();
 keyboard.load();
+const player = new Player(recorder, keyboard);
+
+//
 
 function onKeyPress(event: KeyboardEvent)
 {
     let key = String(event.key).toLocaleLowerCase();
     let time = event.timeStamp;
 
-    let mapper = DefaultKeyboardMapper.mapper;
+    let mapper = DefaultKeyboardMapper.getMapper();
     if(key in mapper) {
         keyboard.setKey(mapper[key]);
         keyboard.play();
+
+        recorder.push(mapper[key], time);
     }
 }
 
@@ -78,23 +28,51 @@ function onClick(event: Event)
 {
     let target = <HTMLElement> event.target;
     let soundKey: (string | undefined) = target.dataset.sound;
+    let time = event.timeStamp;
     
     if(soundKey != undefined) {
         keyboard.setKey(soundKey);
         keyboard.play();
+
+        recorder.push(soundKey, time);
     }
 }
 
 //
 
 let keyboardPanel = document.getElementById("keyboardPanel");
+let recordBtn = document.getElementById("recordBtn");
+let stopBtn = document.getElementById("stopBtn");
+let playBtn = document.getElementById("playBtn");
+
+//
+
 if(keyboardPanel != null) {
     let keys = keyboardPanel.querySelectorAll(".keyboardKey");
-    if(keys != null) {
-        keys.forEach(key => {
-            key.addEventListener("click", onClick);
-        });
-    }
+
+    keys?.forEach(key => {
+        key.addEventListener("click", onClick);
+    });
 }
 
 document.addEventListener("keypress", onKeyPress);
+
+recordBtn?.addEventListener("click", function(event) {
+    if(recorder.state == RecorderState.WAITING) {
+        console.log("RECORD BTN!");
+        recorder.clearChannel();
+        recorder.state = RecorderState.RECORDING;
+    }
+});
+stopBtn?.addEventListener("click", function(event) {
+    if(recorder.state == RecorderState.RECORDING) {
+        console.log("STOP BTN!");
+        recorder.state = RecorderState.WAITING;
+    }
+});
+playBtn?.addEventListener("click", function(event) {
+    if(recorder.state == RecorderState.WAITING) {
+        console.log("PLAY BTN!");
+        player.play();
+    }
+});
